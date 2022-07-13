527694bcc89a68f195ada7899a2cad8f3e720823
dinesh <dinbtechit@gmail.com>
2022-07-13 15:50:56 -0300

    init click-tracker-element application

--- projects/click-tracker-element/src/app/app.module.ts
+++ projects/click-tracker-element/src/app/app.module.ts
@@ -1,27 +1,25 @@
 import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { ClickTrackerModule } from "../../../click-tracker/src/lib/click-tracker.module";
 import { createCustomElement } from "@angular/elements";
 import { ClickTrackerComponent } from "../../../click-tracker/src/lib/click-tracker.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [],
   imports: [
     BrowserModule,
     ClickTrackerModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule {
-  constructor(private injector: Injector) { }
+  constructor(private injector: Injector) {
+  }
 
   ngDoBootstrap() {
-    const ClickTrackerElm = createCustomElement(ClickTrackerComponent, {injector: this.injector});
+    const ClickTrackerElm = createCustomElement(ClickTrackerComponent, { injector: this.injector });
     customElements.define('ct-click-tracker', ClickTrackerElm);
   }
 }
