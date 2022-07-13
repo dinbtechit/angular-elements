0aec86d973d4bbcd6baa25f14a43b1bf7d46c34e
dinesh <dinbtechit@gmail.com>
2022-07-13 15:44:48 -0300

    init click-tracker lib

---
+++ projects/click-tracker/src/lib/click-tracker.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { ClickTrackerComponent } from './click-tracker.component';
+
+
+
+@NgModule({
+  declarations: [
+    ClickTrackerComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    ClickTrackerComponent
+  ]
+})
+export class ClickTrackerModule { }
